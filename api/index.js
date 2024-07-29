1ac01870ea7a3506e0812d3f6e8bfc62cd8416d2
whoappRoom <cabdiwaaxidsiciid100@gmail.com>
2024-07-29 16:27:04 +0300

    added 404.html page

--- api/index.js
+++ api/index.js
@@ -3,6 +3,9 @@ const path = require('path');
 const app = express();
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, '..', 'public', 'index.html')));
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, '..', 'public', '404.html'));
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
