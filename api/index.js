badd8439b927a6c37b63feacf1b91209fb4d1838
Cabdi Waaxid Siciid <158821136+WhoappRoom@users.noreply.github.com>
2025-06-05 13:20:36 +0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -2,4 +2,5 @@ const novax = require('novaxjs2');
 const app = new novax();
 app.serveStatic('public')
 app.get('/', (req, res) => app.sendFile('./index.html', res));
+app.on(404, (req, res) => app.sendFile('./404.html', res));
 app.at(3000, 'localhost', () => console.log('Server is running on port 3000'));
